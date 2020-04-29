3022f2b3b37342ce3609ff1162a4bf6ef077e71b
Zco Developer <>
2020-04-29 22:05:06 +0530

    Support Deployment for push and notification services.

--- .github/secrets/decrypt_secrets.sh
+++ .github/secrets/decrypt_secrets.sh
@@ -3,19 +3,25 @@ set -eo pipefail
 
 echo "Start Import"
 
-gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output .github/secrets/mindlamp2_AppStore_Profile.mobileprovision .github/secrets/mindlamp2_AppStore_Profile.mobileprovision.gpg
 gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output .github/secrets/Certificates.p12 .github/secrets/Certificates.p12.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output .github/secrets/mindlamp2_AppStore_Profile.mobileprovision .github/secrets/mindlamp2_AppStore_Profile.mobileprovision.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output .github/secrets/PushCertificates.p12 .github/secrets/PushCertificates.p12.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output .github/secrets/mindLAMP_2_Notification_Extension_AppStore.mobileprovision .github/secrets/mindLAMP_2_Notification_Extension_AppStore.mobileprovision.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output .github/secrets/mindLAMP_2_Notification_Service_AppStore.mobileprovision .github/secrets/mindLAMP_2_Notification_Service_AppStore.mobileprovision.gpg
 
 echo "Imported"
 
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 
 cp .github/secrets/mindlamp2_AppStore_Profile.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/mindlamp2_AppStore_Profile.mobileprovision
+cp .github/secrets/mindlamp2_AppStore_Profile.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/mindLAMP_2_Notification_Extension_AppStore.mobileprovision
+cp .github/secrets/mindlamp2_AppStore_Profile.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/mindLAMP_2_Notification_Service_AppStore.mobileprovision
 
 echo "Copied"
 
 security create-keychain -p "" build.keychain
 security import .github/secrets/Certificates.p12 -t agg -k ~/Library/Keychains/build.keychain -P "$IOS_KEYS" -A
+security import .github/secrets/PushCertificates.p12 -t agg -k ~/Library/Keychains/build.keychain -P "$IOS_KEYS" -A
 
 security list-keychains -s ~/Library/Keychains/build.keychain
 security default-keychain -s ~/Library/Keychains/build.keychain
