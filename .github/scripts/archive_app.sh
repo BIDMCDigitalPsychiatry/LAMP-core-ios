e95ad71ffaa1ea466a0f45c06d66e52876053669
Jijo Pulikkottil <>
2020-07-23 16:28:54 +0530

    CD fix

--- .github/scripts/archive_app.sh
+++ .github/scripts/archive_app.sh
@@ -4,7 +4,6 @@ set -eo pipefail
 
 xcodebuild -project mindLAMP.xcodeproj \
             -scheme mindLAMP \
-            -sdk iphoneos \
             -configuration Release \
             -archivePath $PWD/build/mindLAMP.xcarchive \
             clean archive | xcpretty
