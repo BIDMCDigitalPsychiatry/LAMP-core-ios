26637a9933b9894762ab85ae58a5c4d7164ed6a6
Jijo Pulikkottil <jijo.pulikkottil@zcocorporation.com>
2020-09-29 22:21:47 +0530

    workflow fix with yanamura

--- .github/scripts/buildnumber.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -eo pipefail
-
-
-git add .
-git commit -m "bump version"
-git remote set-url --push origin https://jijopulikkottil:$GITHUB_TOKEN@github.com/BIDMCDigitalPsychiatry/LAMP-core-ios
-git push origin HEAD
