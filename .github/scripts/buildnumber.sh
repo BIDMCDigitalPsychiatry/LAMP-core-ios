0199d876ebc587c3876d07aa6dc693fab69ee83b
Jijo Pulikkottil <jijo.pulikkottil@zcocorporation.com>
2020-09-29 21:55:24 +0530

    git permission fix

--- .github/scripts/buildnumber.sh
+++ .github/scripts/buildnumber.sh
@@ -4,4 +4,5 @@ set -eo pipefail
 xcrun agvtool new-version -all 1
 git add .
 git commit -m "bump version"
+git remote set-url --push origin https://jijopulikkottil:$GITHUB_TOKEN@github.com/BIDMCDigitalPsychiatry/LAMP-core-ios
 git push origin HEAD
