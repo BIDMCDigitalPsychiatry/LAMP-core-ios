eefb59487866ec8ccfbea70e6951a8345ae5a07a
Jijo Pulikkottil <jijo.pulikkottil@zcocorporation.com>
2020-09-29 22:17:15 +0530

    workflow test1

--- .github/scripts/buildnumber.sh
+++ .github/scripts/buildnumber.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eo pipefail
 
-xcrun agvtool new-version -all 1
+
 git add .
 git commit -m "bump version"
 git remote set-url --push origin https://jijopulikkottil:$GITHUB_TOKEN@github.com/BIDMCDigitalPsychiatry/LAMP-core-ios
