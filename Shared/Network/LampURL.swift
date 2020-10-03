6e02cebcfcd24bfba1129a6174ddee87431a2850
Jijo Pulikkottil <jijo.pulikkottil@zcocorporation.com>
2020-10-03 22:24:24 +0530

    XCConfig for staging

--- Shared/Network/LampURL.swift
+++ Shared/Network/LampURL.swift
@@ -1,32 +1,60 @@
 // mindLAMP
+//https://thoughtbot.com/blog/let-s-setup-your-ios-environments
 
 import Foundation
 
+public enum Environment {
+    
+    // MARK: - Keys
+    enum Keys {
+      enum Plist {
+        static let dashboardURL = "DASHBOARD_URL"
+        static let dashboardAPI = "DASHBOARD_API"
+        static let lampAPI = "LAMP_API"
+      }
+    }
+    
+    // MARK: - Plist
+    private static let infoDictionary: [String: Any] = {
+        guard let dict = Bundle.main.infoDictionary else {
+            fatalError("Plist file not found")
+        }
+        return dict
+    }()
+    
+    // MARK: - Plist values
+    static let dashboardURL: String = {
+        guard let dasgboardURLstring = Environment.infoDictionary[Keys.Plist.dashboardURL] as? String else {
+            fatalError("Root URL not set in plist for this environment")
+        }
+        return dasgboardURLstring
+    }()
+    
+    static let dashboardAPI: String = {
+        guard let endPoint = Environment.infoDictionary[Keys.Plist.dashboardAPI] as? String else {
+            fatalError("API Key not set in plist for this environment")
+        }
+        return endPoint
+    }()
+    
+    static let lampAPI: String = {
+        guard let endPoint = Environment.infoDictionary[Keys.Plist.lampAPI] as? String else {
+            fatalError("API Key not set in plist for this environment")
+        }
+        return endPoint
+    }()
+}
+
 struct LampURL {
     //static let test = "http://127.0.0.1:5000/login"
-    static let dashboardlive = "https://dashboard.lamp.digital/#/"//"https://dashboard-staging.lamp.digital" ////TODO:
+    //static let dashboardlive = Environment.dashboardURL
     static var dashboardDigital: URL {
         return URL(string: dashboardDigitalURLText)!
     }
-    static let dashboardDigitalURLText = (UserDefaults.standard.launchURL ?? dashboardlive)// + "?a="
+    static let dashboardDigitalURLText = (UserDefaults.standard.launchURL ?? Environment.dashboardURL)// + "?a="
     //static let loginLocalHost = "http://127.0.0.1:5000/login"
     static let logsDigital = "https://logs.lamp.digital"
-    static let dashboardURL = "http://52.66.237.209:9092"
-    static let lampAPI = "https://api.lamp.digital"
-
+    static let dashboardURL = Environment.dashboardAPI
+    static let lampAPI = Environment.lampAPI
+    
 }
-
-
-//struct LampURL {
-//
-//    static let dashboardlive = "http://52.66.237.209:9094/#/"//"https://dashboard-staging.lamp.digital" ////TODO:
-//    static var dashboardDigital: URL {
-//        return URL(string: dashboardDigitalURLText)!
-//    }
-//    static let dashboardDigitalURLText = (UserDefaults.standard.launchURL ?? dashboardlive)// + "?a="
-//    //static let loginLocalHost = "http://127.0.0.1:5000/login"
-//    static let logsDigital = "https://logs.lamp.digital"
-//    static let dashboardURL = "http://52.66.237.209:9092"
-//    static let lampAPI = "http://52.66.237.209:9093"
-//
-//}
