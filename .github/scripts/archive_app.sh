ab190ce790ca4d79e9b73c3f977a9b3e86492a28
Jijo Pulikkottil <jijo.pulikkottil@zcocorporation.com>
2022-04-13 21:48:04 +0530

    profiles replaced

--- .github/scripts/archive_app.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -eo pipefail
-
-xcodebuild -project mindLAMP.xcodeproj \
-            -scheme mindLAMP \
-            -configuration Release \
-            -archivePath $PWD/build/mindLAMP.xcarchive \
-            clean archive | xcpretty
--- .github/secrets/decrypt_secrets.sh
+++ .github/secrets/decrypt_secrets.sh
@@ -5,23 +5,23 @@ echo "Start Import"
 
 gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output .github/secrets/Certificates.p12 .github/secrets/Certificates.p12.gpg
 
-gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output .github/secrets/mindLAMP_2.mobileprovision .github/secrets/mindLAMP_2.mobileprovision.gpg
-gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output .github/secrets/mindLAMP_2_Notification_Extension_AppStore.mobileprovision .github/secrets/mindLAMP_2_Notification_Extension_AppStore.mobileprovision.gpg
-gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output .github/secrets/mindLAMP_2_Notification_Service_AppStore.mobileprovision .github/secrets/mindLAMP_2_Notification_Service_AppStore.mobileprovision.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output .github/secrets/mindLAMP2.mobileprovision .github/secrets/mindLAMP2.mobileprovision.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output .github/secrets/mindLAMP2_Custom_Notification_Extension_AppStore.mobileprovision .github/secrets/mindLAMP2_Custom_Notification_Extension_AppStore.mobileprovision.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output .github/secrets/mindLAMP2_Notification_Service_AppStore.mobileprovision .github/secrets/mindLAMP2_Notification_Service_AppStore.mobileprovision.gpg
 
-gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output .github/secrets/mindLAMP2_WatchApp_Extension.mobileprovision .github/secrets/mindLAMP2_WatchApp_Extension.mobileprovision.gpg
-gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output .github/secrets/mindLAMP2_WatchApp.mobileprovision .github/secrets/mindLAMP2_WatchApp.mobileprovision.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output .github/secrets/mindLAMP2_WatchApp_Extension_AppStore.mobileprovision .github/secrets/mindLAMP2_WatchApp_Extension_AppStore.mobileprovision.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output .github/secrets/mindLAMP2_WatchApp_AppStore.mobileprovision .github/secrets/mindLAMP2_WatchApp_AppStore.mobileprovision.gpg
 
 echo "Imported"
 
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 
-cp .github/secrets/mindLAMP_2.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/mindLAMP_2.mobileprovision
-cp .github/secrets/mindLAMP_2_Notification_Extension_AppStore.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/mindLAMP_2_Notification_Extension_AppStore.mobileprovision
-cp .github/secrets/mindLAMP_2_Notification_Service_AppStore.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/mindLAMP_2_Notification_Service_AppStore.mobileprovision
+cp .github/secrets/mindLAMP2.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/mindLAMP2.mobileprovision
+cp .github/secrets/mindLAMP2_Custom_Notification_Extension_AppStore.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/mindLAMP2_Custom_Notification_Extension_AppStore.mobileprovision
+cp .github/secrets/mindLAMP2_Notification_Service_AppStore.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/mindLAMP2_Notification_Service_AppStore.mobileprovision
 
-cp .github/secrets/mindLAMP2_WatchApp_Extension.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/mindLAMP2_WatchApp_Extension.mobileprovision
-cp .github/secrets/mindLAMP2_WatchApp.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/mindLAMP2_WatchApp.mobileprovision
+cp .github/secrets/mindLAMP2_WatchApp_Extension_AppStore.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/mindLAMP2_WatchApp_Extension_AppStore.mobileprovision
+cp .github/secrets/mindLAMP2_WatchApp_AppStore.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/mindLAMP2_WatchApp_AppStore.mobileprovision
 
 echo "Copied"
 
