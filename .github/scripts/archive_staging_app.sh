a1a47370988f0aebe072f7cc957991a044b01b56
jijo.pulikkottil <jijo.pulikkottil@zcocorporation.com>
2025-03-14 13:52:12 +0530

    Project settings changed to auto signing.

--- .github/scripts/archive_staging_app.sh
+++ .github/scripts/archive_staging_app.sh
@@ -6,5 +6,4 @@ xcodebuild -project mindLAMP.xcodeproj \
             -scheme mindLAMP\ \(Staging\) \
             -configuration Release\ \(Staging\) \
             -archivePath $PWD/build/mindLAMP.xcarchive \
-            -destination "generic/platform=iOS" \
             clean archive | xcpretty
