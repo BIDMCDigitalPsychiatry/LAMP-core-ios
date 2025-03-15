42169db2d1012b6f4efb0cf5fbeff3e16e4382a0
jijo.pulikkottil <jijo.pulikkottil@zcocorporation.com>
2025-03-15 11:34:24 +0530

    publis to testflight script fix

--- .github/scripts/publish_testflight.sh
+++ .github/scripts/publish_testflight.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 set -eo pipefail
-echo "${{ secrets.APPSTORE_API_KEY_BASE64 }}" | base64 --decode > AuthKey.p8
-export APPSTORE_KEY_ID = "V7878248C8"
-export APPSTORE_ISSUER_ID = "69a6de90-0c7b-47e3-e053-5b8c7c11a4d1"
+echo "$APPSTORE_API_KEY_BASE64" | base64 --decode > AuthKey.p8
 xcrun altool --upload-app -t ios -f build/mindLAMP\ 2.ipa \
       --apiKey $APPSTORE_KEY_ID \
       --apiIssuer $APPSTORE_ISSUER_ID \
       --apiKeyPath AuthKey.p8 \
       --verbose
+rm -f AuthKey.p8
