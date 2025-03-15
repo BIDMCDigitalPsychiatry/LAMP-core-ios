976bab2515b3c7b7a4076ea9631b7d4320f83cbd
jijo.pulikkottil <jijo.pulikkottil@zcocorporation.com>
2025-03-15 16:05:39 +0530

    github action script optimization

--- .github/scripts/publish_testflight.sh
+++ .github/scripts/publish_testflight.sh
@@ -3,10 +3,11 @@
 set -eo pipefail
 
 mkdir -p ~/private_keys
+APPSTORE_API_KEY_PATH="~/private_keys/$APPSTORE_API_KEY_FILENAME"
 echo "$APPSTORE_API_KEY_BASE64" | base64 --decode > $APPSTORE_API_KEY_PATH
 xcrun altool --upload-app -t ios -f build/mindLAMP\ 2.ipa \
       --apiKey $APPSTORE_KEY_ID \
       --apiIssuer $APPSTORE_ISSUER_ID \
       --apiKeyPath $APPSTORE_API_KEY_PATH \
       --verbose
-rm -f $APPSTORE_API_KEY_PATH
+rm -f "$APPSTORE_API_KEY_PATH"
