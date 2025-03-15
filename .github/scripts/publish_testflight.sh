ff133c728f4e8bd23f341d079eff1c77b21d6045
jijo.pulikkottil <jijo.pulikkottil@zcocorporation.com>
2025-03-15 11:54:38 +0530

    script bug fix

--- .github/scripts/publish_testflight.sh
+++ .github/scripts/publish_testflight.sh
@@ -5,6 +5,6 @@ echo "$APPSTORE_API_KEY_BASE64" | base64 --decode > ~/private_keys/AuthKey_V7878
 xcrun altool --upload-app -t ios -f build/mindLAMP\ 2.ipa \
       --apiKey $APPSTORE_KEY_ID \
       --apiIssuer $APPSTORE_ISSUER_ID \
-      --apiKeyPath AuthKey.p8 \
+      --apiKeyPath ~/private_keys/AuthKey_V7878248C8.p8 \
       --verbose
 rm -f ~/private_keys/AuthKey_V7878248C8.p8
