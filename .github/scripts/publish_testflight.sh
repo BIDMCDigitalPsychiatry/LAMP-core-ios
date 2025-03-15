2f499b9c60d8c731109b85e686e472e7c0ca3444
jijo.pulikkottil <jijo.pulikkottil@zcocorporation.com>
2025-03-15 13:15:55 +0530

    github action script fix - create directory if not exist

--- .github/scripts/publish_testflight.sh
+++ .github/scripts/publish_testflight.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 set -eo pipefail
+
+mkdir -p ~/private_keys
 echo "$APPSTORE_API_KEY_BASE64" | base64 --decode > ~/private_keys/AuthKey_V7878248C8.p8
 xcrun altool --upload-app -t ios -f build/mindLAMP\ 2.ipa \
       --apiKey $APPSTORE_KEY_ID \
