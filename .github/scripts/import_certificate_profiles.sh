c133bb013ed951cd9a18286cee26dd6241e2601e
jijo.pulikkottil <jijo.pulikkottil@zcocorporation.com>
2025-03-15 13:57:01 +0530

    grouping script to new files

--- .github/scripts/import_certificate_profiles.sh
+++ .github/scripts/import_certificate_profiles.sh
@@ -2,7 +2,7 @@
 
 set -eo pipefail
 
-# create variables
+echo "create variables"
 CERTIFICATE_PATH=$RUNNER_TEMP/build_certificate.p12
 PP_PATH1=$RUNNER_TEMP/build_pp1.mobileprovision
 PP_PATH2=$RUNNER_TEMP/build_pp2.mobileprovision
@@ -12,7 +12,7 @@ PP_PATH5=$RUNNER_TEMP/build_pp5.mobileprovision
 PP_PATH6=$RUNNER_TEMP/build_pp6.mobileprovision
 KEYCHAIN_PATH=$RUNNER_TEMP/app-signing.keychain-db
 
-# import certificate and provisioning profile from secrets
+echo "import certificate and provisioning profile from secrets"
 echo -n "$BUILD_CERTIFICATE_BASE64" | base64 --decode -o $CERTIFICATE_PATH
 echo -n "$BUILD_PROVISION_PROFILE1_BASE64" | base64 --decode -o $PP_PATH1
 echo -n "$BUILD_PROVISION_PROFILE2_BASE64" | base64 --decode -o $PP_PATH2
@@ -21,15 +21,20 @@ echo -n "$BUILD_PROVISION_PROFILE4_BASE64" | base64 --decode -o $PP_PATH4
 echo -n "$BUILD_PROVISION_PROFILE5_BASE64" | base64 --decode -o $PP_PATH5
 echo -n "$BUILD_PROVISION_PROFILE6_BASE64" | base64 --decode -o $PP_PATH6
 
-# create temporary keychain
+echo "create temporary keychain"
 security create-keychain -p "$KEYCHAIN_PASSWORD" $KEYCHAIN_PATH
 security set-keychain-settings -lut 21600 $KEYCHAIN_PATH
 security unlock-keychain -p "$KEYCHAIN_PASSWORD" $KEYCHAIN_PATH
 
-# import certificate to keychain
+echo "import certificate to keychain"
 security import $CERTIFICATE_PATH -P "$IOS_KEYS" -A -t cert -f pkcs12 -k $KEYCHAIN_PATH
 security list-keychain -d user -s $KEYCHAIN_PATH
 
-# apply provisioning profile
+echo "apply provisioning profile"
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
-cp $PP_PATH ~/Library/MobileDevice/Provisioning\ Profiles
+cp $PP_PATH1 ~/Library/MobileDevice/Provisioning\ Profiles
+cp $PP_PATH2 ~/Library/MobileDevice/Provisioning\ Profiles
+cp $PP_PATH3 ~/Library/MobileDevice/Provisioning\ Profiles
+cp $PP_PATH4 ~/Library/MobileDevice/Provisioning\ Profiles
+cp $PP_PATH5 ~/Library/MobileDevice/Provisioning\ Profiles
+cp $PP_PATH6 ~/Library/MobileDevice/Provisioning\ Profiles
