42b0d7a27668a034d6ec01d705f1f7aeaa85216d
jijo.pulikkottil <jijo.pulikkottil@zcocorporation.com>
2025-03-16 21:28:21 +0530

    github action CI - set iphone simulator name dynamically

--- .github/scripts/test_project.sh
+++ .github/scripts/test_project.sh
@@ -2,4 +2,6 @@
 
 set -eo pipefail
 
-xcodebuild test -project mindLAMP.xcodeproj -scheme mindLAMP -destination 'platform=iOS Simulator,OS=latest'
+DEVICE=$(xcrun simctl list devices available | grep -m 1 "iPhone" | awk -F '[()]' '{print $2}')
+echo "Selected device: $DEVICE"
+xcodebuild test -project mindLAMP.xcodeproj -scheme mindLAMP -destination "platform=iOS Simulator,id=$DEVICE"
